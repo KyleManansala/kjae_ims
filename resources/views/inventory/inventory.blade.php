80e61318dabb2c7623c70639b9244a58c87e3d00
Jericho Lardizabal <jericholardizabal2002@gmail.com>
2023-10-21 21:17:06 +0800

    added admin sidebar

---
+++ resources/views/inventory/inventory.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <x-header title="Inventory" />
+    </x-slot>
+
+
+    
+</x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
-
+const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
@@ -17,5 +20,14 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+    forms,
+        plugin(({ addVariant, e }) => {
+            addVariant('sidebar-expanded', ({ modifySelectors, separator }) => {
+                modifySelectors(({ className }) => `.sidebar-expanded .${e(`sidebar-expanded${separator}${className}`)}`);
+            });
+        }),
+    
+    
+    ],
 };
