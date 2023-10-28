b70f1728554d3a188a58377b991a9732636aa362
KyleManansala <hustlerkyle13@gmail.com>
2023-10-28 18:51:33 +0800

    routing changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InventoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,9 +23,29 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/inventory', function () {
-    return view('inventory.inventory');
-})->middleware(['auth', 'verified'])->name('inventory');
+Route::get('/inventory', [InventoryController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('inventory');
+
+Route::post('/inventory/search', [InventoryController::class, 'search'])
+    ->middleware(['auth', 'verified'])
+    ->name('inventory.search');
+
+Route::post('/inventory/add', [InventoryController::class, 'store'])
+    ->middleware(['auth', 'verified'])
+    ->name('inventory.add');
+
+Route::delete('/inventory/delete/{id}', [InventoryController::class, 'delete'])
+    ->middleware(['auth', 'verified'])
+    ->name('inventory.delete');
+
+Route::post('/inventory/update/{id}', [InventoryController::class, 'update'])
+    ->middleware(['auth', 'verified'])
+    ->name('inventory.update');
+
+Route::get('/reports', function () {
+    return view('inventory.reports');
+})->middleware(['auth', 'verified'])->name('reports');
 
 Route::get('/reports', function () {
     return view('inventory.reports');
@@ -34,11 +55,12 @@ Route::get('/weather', function(){
     return view('weather.weather');
 })->middleware(['auth', 'verified'])->name('weather');
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
