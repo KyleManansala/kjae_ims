52be955a104586d55282f6bab87c9acb3e27f3df
Jericho Lardizabal <jericholardizabal2002@gmail.com>
2023-11-17 00:33:26 +0800

    added x-cloak and new colors

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class=" bg-white dark:bg-slate-800 shadow sticky top-16">
+<header class=" bg-slate-800 dark:bg-slate-800 shadow sticky top-16">
     <div class="max-w-7xl mx-auto flex gap-2 items-center py-6 px-4 sm:px-6 lg:px-8">    
         <h2 class="font-semibold text-xl text-white leading-tight">
             {{ $title ?? $slot }}
